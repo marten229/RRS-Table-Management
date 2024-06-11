f0cdf7a410c2ceb6a521b102ff849c327cea7186
Marten Schulz <ms@marten-schulz.de>
2024-06-11 19:41:26 +0200

    In der Tisch Detail Ansicht können nun auch direkt neue Reservierungen hinzugefügt werden

--- views.py
+++ views.py
@@ -1,86 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
-from .models import RestaurantLogin, Table, SeatingPlan, StaffSchedule
+from .models import Table, SeatingPlan, StaffSchedule
 from django.utils import timezone
 from RestaurantManagement.models import Restaurant
 from ReservationManagement.models import Reservation
 from UserManagement.models import User
-from datetime import timedelta
 import datetime
-from django.http import JsonResponse
-from datetime import datetime, date, time, timedelta
-import logging
-
-def tishmanage(request):
-    all_items = Restaurant.objects.all()
-    all_logins = RestaurantLogin.objects.all()
-    return render(request, 'tischverwaltung.html', {
-        'all_items': all_items,
-        'all_logins': all_logins
-    })
-
-def restoruantanzeigen(request):
-    all_items = Restaurant.objects.all()
-    return render(request, 'tischverwaltung.html', {
-        'all_items': all_items,
-    })
-
-def anzeigen(request):
-    selected_restaurant_name = request.GET.get('restaurant', '') # Restaurantnamen aus GET-Parameter extrahieren
-    selected_login = RestaurantLogin.objects.filter(name=selected_restaurant_name).first()
-    return render(request, 'anzeige.html', {'selected_login': selected_login,'selected_restaurant_name': selected_restaurant_name})
-
-def tisch_details_one(request):
-    today = timezone.now().date()
-    selected_restaurant_name = request.GET.get('restaurant', '') # Restaurantnamen aus GET-Parameter extrahieren
-    selected_login = RestaurantLogin.objects.filter(name=selected_restaurant_name).first()
-    kunden = Table.objects.filter(guests__lte=4, date__gte=today)
-    return render(request, 'tisch_details1.html', {'kunden': kunden, 'selected_login': selected_login})
-
-def tisch_details_two(request):
-    today = timezone.now().date()
-    selected_restaurant_name = request.GET.get('restaurant', '') # Restaurantnamen aus GET-Parameter extrahieren
-    selected_login = RestaurantLogin.objects.filter(name=selected_restaurant_name).first()
-    kunden = Table.objects.filter(guests__gt=4, guests__lte=6, date__gte=today)
-    return render(request, 'tisch_details2.html', {'kunden': kunden, 'selected_login': selected_login})
-
-def tisch_details_three(request):
-    today = timezone.now().date()
-    selected_restaurant_name = request.GET.get('restaurant', '') # Restaurantnamen aus GET-Parameter extrahieren
-    selected_login = RestaurantLogin.objects.filter(name=selected_restaurant_name).first()
-    kunden = Table.objects.filter(guests__gt=6, guests__lte=8, date__gte=today)
-    return render(request, 'tisch_details3.html', {'kunden': kunden, 'selected_login': selected_login})
-
-def tisch_details_four(request):
-    today = timezone.now().date()
-    selected_restaurant_name = request.GET.get('restaurant', '') # Restaurantnamen aus GET-Parameter extrahieren
-    selected_login = RestaurantLogin.objects.filter(name=selected_restaurant_name).first()
-    kunden = Table.objects.filter(guests__gt=8, date__gte=today)
-    return render(request, 'tisch_details4.html', {'kunden': kunden, 'selected_login': selected_login})
-
-def all_tables(request):
-    today = timezone.now().date()
-    selected_restaurant_name = request.GET.get('restaurant', '') # Restaurantnamen aus GET-Parameter extrahieren
-    selected_login = RestaurantLogin.objects.filter(name=selected_restaurant_name).first()
-    kunden = Table.objects.all()
-    return render(request, 'all_tables.html', {'kunden': kunden, 'selected_login': selected_login})
-
-def all_tish(request, pk):
-    today = timezone.now().date()
-    selected_restaurant = get_object_or_404(Restaurant, pk=pk)
-    table_status = []
-
-    if selected_restaurant:
-        all_tables = Table.objects.filter(restaurant=selected_restaurant)
-
-        #for table_number in range(1, restaurant_count+1):
-        #    is_occupied = all_tables.filter(table_number=table_number, date__gte=today).exists()
-        #    status = 'Besetzt' if is_occupied else 'Verfügbar'
-        #    table_status.append({'table_number': table_number, 'status': status})
-    else:
-        selected_restaurant = None
-
-    return render(request, 'alltish.html', {'table_status': table_status, 'selected_restaurant': selected_restaurant})
+from datetime import datetime, time, timedelta
+from .forms import AssignReservationForm
 
 def create_seating_plan(restaurant, date):
     reservations = Reservation.objects.filter(restaurant=restaurant, datum=date).order_by('uhrzeit')
@@ -166,14 +92,47 @@ def table_list(request, restaurant_id):
 def table_detail(request, table_id):
     table = get_object_or_404(Table, id=table_id)
     reservations = SeatingPlan.objects.filter(table=table).select_related('reservation')
-
+    
     table_availability = table.is_available()
 
-    #logger.debug(f"Table {table.id} availability in view: {table_availability}")
-    print(f"Table {table.id} availability in view: {table_availability}")
+    if request.method == 'POST':
+        if 'assign_reservation' in request.POST:
+            form = AssignReservationForm(request.POST)
+            if form.is_valid():
+                existing_reservation = form.cleaned_data.get('vorhandene_reservierung')
+                if existing_reservation:
+                    reservation = existing_reservation
+                else:
+                    reservation = Reservation.objects.create(
+                        restaurant=table.restaurant,
+                        name=form.cleaned_data.get('neue_reservierung_name'),
+                        email=form.cleaned_data.get('neue_reservierung_email'),
+                        telefon_nummer=form.cleaned_data.get('neue_reservierung_telefon'),
+                        datum=form.cleaned_data.get('neue_reservierung_datum'),
+                        uhrzeit=form.cleaned_data.get('neue_reservierung_uhrzeit'),
+                        dauer=form.cleaned_data.get('neue_reservierung_dauer'),
+                        anzahl_an_gästen=form.cleaned_data.get('neue_reservierung_gaeste')
+                    )
+                
+                seating_plan = SeatingPlan.objects.create(
+                    restaurant=table.restaurant,
+                    table=table,
+                    reservation=reservation,
+                    start_time=reservation.uhrzeit,
+                    end_time=(datetime.combine(datetime.min, reservation.uhrzeit) + timedelta(minutes=reservation.dauer)).time()
+                )
+                return redirect('table_detail', table_id=table.id)
+        elif 'remove_reservation' in request.POST:
+            seating_plan_id = request.POST.get('seating_plan_id')
+            seating_plan = get_object_or_404(SeatingPlan, id=seating_plan_id)
+            seating_plan.delete()
+            return redirect('table_detail', table_id=table.id)
+    else:
+        form = AssignReservationForm()
 
     return render(request, 'table_detail.html', {
         'table': table,
         'reservations': reservations,
-        'is_available': table_availability
+        'is_available': table_availability,
+        'form': form
     })
