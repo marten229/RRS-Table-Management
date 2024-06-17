1e3b28ffe6df251c001daeeb9c6a644707ff1bf6
Marten Schulz <ms@marten-schulz.de>
2024-06-17 21:12:15 +0200

    refactor: Remove unused URLs in urls.py

--- urls.py
+++ urls.py
@@ -14,11 +14,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from TableManagement import views
 urlpatterns = [
-    #path('generate_and_view_plans/<int:pk>/', views.generate_and_view_plans, name='generate_and_view_plans'),
-    #path('restaurants/<int:restaurant_id>/tables/', views.table_list, name='table_list'),
-    #path('tables/<int:table_id>/', views.table_detail, name='table_detail'),
+
 ]
